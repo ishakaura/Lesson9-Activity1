05971fa26a37cebd64267a32618891438ac054d6
Isha <ishakaura73@gmail.com>
2026-06-14 15:02:18 +0530

    lesson9m activity 1

---
+++ main.py
@@ -0,0 +1,13 @@
+# Typography Example
+
+css_code = """
+h1 {
+    font-family: Arial;
+    font-size: 30px;
+    font-weight: bold;
+    color: blue;
+    text-align: center;
+}
+"""
+
+print(css_code)
